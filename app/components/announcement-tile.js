e75bd4046091eeb91a374fd3e57c920f20f27252
eddie duro and jordan meier <jbirdmeier@gmail.com>
2016-03-28 13:33:01 -0700

    allow user to delete announcements

--- app/components/announcement-tile.js
+++ app/components/announcement-tile.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    delete(announcement) {
+      if (confirm('You sure you want to delete this announcement?')) {
+        this.sendAction('destroyAnnouncement', announcement);
+      }
+    }
+  }
 });
